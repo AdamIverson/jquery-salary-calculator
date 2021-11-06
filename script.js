630cfb8e319f506bb745885b76a48942063842ed
Adam Iverson <aciverso@gmail.com>
2021-11-06 11:26:38 -0500

    click function up and posting undefined to DOM

--- script.js
+++ script.js
@@ -12,11 +12,35 @@ function onReady() {
     // direct data to calculation functions
     //append calc functions data to DOM
     // add red background to totalMonthlyCost if > $20k
+    $('#submit-button').on('click', handleSubmitClick);
 } // end onReady
 
 function renderEmployees() {
-    console.log('inside input');
+    $('#employee-table-body').empty();
 
-    
-    
+    for (let employee of employees) {
+        let newTableRow = `
+        <tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.idNumber}</td>
+            <td>${employee.jobTitle}</td>
+            <td>${employee.annualSalary}</td>
+        <tr>
+        `;
+
+        $('#employee-table-body').append(newTableRow);
+    } // end newTableRow
 } // end renderEmployees
+
+function handleSubmitClick() {
+    let newFirstName = $('#first-name-input').val();
+    
+    let newEmployee = {
+        name: newFirstName,
+    };
+
+    employees.push(newEmployee);
+    $('#first-name-input').val('');
+    renderEmployees(employees);
+}
