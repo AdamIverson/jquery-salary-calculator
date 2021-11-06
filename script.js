bd010fd1f71a2239f26edc7d8e007f4a49eeea22
Adam Iverson <aciverso@gmail.com>
2021-11-06 11:10:21 -0500

    document.ready and onReady cemented

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('hello');
+$(document).ready(onReady);
+
+let employees = [];
+
+
+function onReady() {
+    // collect data from input fields via submit button
+    renderEmployees(employees);
+    // empty input fields
+    // store input data in variables
+    // create array of employee objects
+    // direct data to calculation functions
+    //append calc functions data to DOM
+    // add red background to totalMonthlyCost if > $20k
+} // end onReady
+
+function renderEmployees() {
+    console.log('inside input');
+
+    
+    
+} // end renderEmployees
