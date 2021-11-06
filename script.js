efaa51a8cfe8ac47e27d4f2dbc0ca6fc29328e12
Adam Iverson <aciverso@gmail.com>
2021-11-06 11:54:16 -0500

    onClick function working

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let employees = [];
 function onReady() {
     // collect data from input fields via submit button
     renderEmployees(employees);
+    renderTotalMonthlyExpenses(employees);
     // empty input fields
     // store input data in variables
     // create array of employee objects
@@ -15,7 +16,7 @@ function onReady() {
     $('#submit-button').on('click', handleSubmitClick);
 } // end onReady
 
-function renderEmployees() {
+function renderEmployees(employees) {
     $('#employee-table-body').empty();
 
     for (let employee of employees) {
@@ -35,12 +36,25 @@ function renderEmployees() {
 
 function handleSubmitClick() {
     let newFirstName = $('#first-name-input').val();
-    
+    let newLastName = $('#last-name-input').val();
+    let newIdNumber = $('#id-input').val();
+    let newJobTitle = $('#job-title-input').val();
+    let newAnnualSalary = $('#annual-salary-input').val();
+
     let newEmployee = {
-        name: newFirstName,
+        firstName: newFirstName,
+        lastName: newLastName,
+        idNumber: newIdNumber,
+        jobTitle: newJobTitle,
+        annualSalary: newAnnualSalary
     };
 
     employees.push(newEmployee);
     $('#first-name-input').val('');
+    $('#last-name-input').val('');
+    $('#id-inputt').val('');
+    $('#job-title-input').val('');
+    $('#annual-salary-input').val('');
     renderEmployees(employees);
+    renderTotalMonthlyExpenses(employees);
 }
