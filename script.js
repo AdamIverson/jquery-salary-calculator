ea7e49c5740cbcc96770ac97dc213eb15ae66388
Adam Iverson <aciverso@gmail.com>
2021-11-06 18:08:39 -0500

    monthly total functional in dom

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let employees = [];
 function onReady() {
     // collect data from input fields via submit button
     renderEmployees(employees);
-    // renderTotalMonthlyExpenses(employees);
+    renderTotalMonthlyExpenses(employees);
     // empty input fields
     // store input data in variables
     // create array of employee objects
@@ -14,7 +14,36 @@ function onReady() {
     //append calc functions data to DOM
     // add red background to totalMonthlyCost if > $20k
     $('#submit-button').on('click', handleSubmitClick);
+    $('.delete-button').on('click', handleDeleteClick);
 } // end onReady
+
+function handleSubmitClick() {
+
+    let newEmployee = {
+        firstName: $('#first-name-input').val(),
+        lastName: $('#last-name-input').val(),
+        idNumber: $('#id-input').val(),
+        jobTitle: $('#job-title-input').val(),
+        annualSalary: $('#annual-salary-input').val()
+    };
+
+    employees.push(newEmployee);
+    $('#first-name-input').val('');
+    $('#last-name-input').val('');
+    $('#id-input').val('');
+    $('#job-title-input').val('');
+    $('#annual-salary-input').val('');
+    renderEmployees(employees);
+    renderTotalMonthlyExpenses(employees);
+} // end handleSubmitClick
+
+function handleDeleteClick() {
+    console.log('in delete');
+    
+    $('#employee-table-body')
+    .last()
+    .remove()
+}
 
 function renderEmployees(employees) {
     $('#employee-table-body').empty();
@@ -35,27 +64,15 @@ function renderEmployees(employees) {
     } // end newTableRow
 } // end renderEmployees
 
-function handleSubmitClick() {
-    let newFirstName = $('#first-name-input').val();
-    let newLastName = $('#last-name-input').val();
-    let newIdNumber = $('#id-input').val();
-    let newJobTitle = $('#job-title-input').val();
-    let newAnnualSalary = $('#annual-salary-input').val();
+function renderTotalMonthlyExpenses(employees) {
+    $('#monthly-total').empty();
 
-    let newEmployee = {
-        firstName: newFirstName,
-        lastName: newLastName,
-        idNumber: newIdNumber,
-        jobTitle: newJobTitle,
-        annualSalary: newAnnualSalary
-    };
+    let monthlyTotal = 0;
 
-    employees.push(newEmployee);
-    $('#first-name-input').val('');
-    $('#last-name-input').val('');
-    $('#id-inputt').val('');
-    $('#job-title-input').val('');
-    $('#annual-salary-input').val('');
-    renderEmployees(employees);
-    renderTotalMonthlyExpenses(employees);
+    for (let employee of employees) {
+        let monthlyCost = employee.annualSalary/12
+
+        monthlyTotal += monthlyCost;
+    }
+    $('#monthly-total').append('Total Monthly $', monthlyTotal.toFixed());
 }
